495fc9f1ffbfe361ced72dde623d5b9419f34ac8
YuhanLin-Git <57867183+findoctorlin@users.noreply.github.com>
2021-09-29 11:00:46 +0200

    Leetcode 350

---
+++ 350Intersection of Two Arrays II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> record;
+        vector<int> res;
+        
+        for(int i = 0; i < nums1.size(); ++i) {
+            record[nums1[i]]++;
+        }
+        
+        for(int i = 0; i < nums2.size(); ++i) {
+            if(record[nums2[i]] != 0) {
+                res.push_back(nums2[i]);
+                record[nums2[i]]--;
+            }
+        }
+        return res;
+    }
+};
