47e211b6db09548e963b12eb97ab5c00986ce4ce
YuhanLin-Git <57867183+findoctorlin@users.noreply.github.com>
2021-10-02 00:16:08 +0200

    Longest Substring Without Repeating Characters

--- Leetcode3.cpp
+++ Leetcode3.cpp
@@ -3,14 +3,17 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<char, int> m;
         int maxlen = 0;
-        for(int i = 0, j = 0; i < s.size(); ++i) {
-            m[s[i]]++;
-            while(m[s[i]] > 1) {
-                m[s[j]]--;
-                j++;
+        for(int i = 0, j = 0; j < s.size(); j++){
+            m[s[j]]++;
+            while(m[s[j]] > 1) {
+                // m[s[i++]]--;    //左边界向右走，直到当前窗没有重复字符
+                m[s[i]]--;
+                i++;
             }
-            maxlen = max(maxlen, i-j+1);
+            maxlen = max(maxlen, j - i + 1);
         }
         return maxlen;
     }
 };
+
+//双指针：i负责留在原地，j负责遍历
